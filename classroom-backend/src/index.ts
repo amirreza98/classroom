18de4f6aba0ac98f9ee8b394d7cb412c9a1ee603
amirreza azemati <amirreza.azemati@gmail.com>
2026-02-19 15:37:46 +0100

    initial commit for backend

---
+++ classroom-backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get('/',(req, res) => {
+    res.send('Hello, welcome to the Classroom');
+});
+ 
+app.listen(PORT, () => { console.log(`Server is running at http://localhost:${PORT}`);
+});
