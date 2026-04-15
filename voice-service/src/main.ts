dd0ed3f4cb553cde07867dbd2abd0733b7794f04
amirreza azemati <amirreza.azemati@gmail.com>
2026-04-15 17:30:49 +0200

    disble defualt bodyParser

--- voice-service/src/main.ts
+++ voice-service/src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: false,
+  });
 
   // Increase body size limits to handle PDF uploads
   app.use(express.json({ limit: '50mb' }));
