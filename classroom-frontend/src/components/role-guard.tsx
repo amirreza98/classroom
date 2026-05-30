ae0cacc6ba7db5d08f412dadf90db9520d432e08
amirreza azemati <amirreza.azemati@gmail.com>
2026-05-30 23:29:39 +0200

    waiting sesion at initial

--- classroom-frontend/src/components/role-guard.tsx
+++ classroom-frontend/src/components/role-guard.tsx
@@ -1,3 +1,4 @@
+// components/role-guard.tsx
 import { useSession } from "@/lib/auth-client";
 import { Navigate, Outlet } from "react-router";
 
@@ -6,13 +7,10 @@ type Props = {
 }
 
 export function RoleGuard({ allowedRoles }: Props) {
-  const { data: session, isPending } = useSession();
-
-  if (isPending) return null;
-
+  const { data: session } = useSession();
   const role = session?.user?.role as string;
 
-  if (!role || !allowedRoles.includes(role as any)) {
+  if (!allowedRoles.includes(role as any)) {
     return <Navigate to="/" replace />;
   }
 
