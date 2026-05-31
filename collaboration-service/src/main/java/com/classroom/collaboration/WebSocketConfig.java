fce3af867b0b35c9a750838dda18ba21f69d513d
amirreza azemati <amirreza.azemati@gmail.com>
2026-05-31 20:54:41 +0200

    fix: move main class to correct package for WebSocket registration

--- collaboration-service/src/main/java/com/classroom/collaboration/WebSocketConfig.java
+++ collaboration-service/src/main/java/com/classroom/collaboration/WebSocketConfig.java
@@ -1,11 +1,13 @@
 package com.classroom.collaboration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -18,6 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        log.info("Registering WebSocket endpoint at /collaboration");
         registry.addEndpoint("/collaboration")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
