1d815bea75094114b1be0770cfa98cfa1ea892e2
amirreza azemati <amirreza.azemati@gmail.com>
2026-06-10 15:40:25 +0200

    add rest endpoint for classes in colabration

--- collaboration-service/src/main/java/com/classroom/collaboration/CollaborativeFileRepository.java
+++ collaboration-service/src/main/java/com/classroom/collaboration/CollaborativeFileRepository.java
@@ -1,7 +1,7 @@
 package com.classroom.collaboration;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 
 public interface CollaborativeFileRepository extends JpaRepository<CollaborativeFile, Long> {
